6a8392a16b8fe37518e09f6740bcb54fca7427fb
pclinux <pclinux@local.co>
2022-11-05 12:57:49 -0500

    modificando y agregando estilos al header

--- web/views/modules/header.php
+++ web/views/modules/header.php
@@ -1,45 +1,45 @@
 
 <header>
-    <div class="p-0 container my-4 top">
+    <div class="container top">
 
-    <div class="row">
+    <div class="row header__elment">
 
-        <div class="my-1 col-sm-12 col-md-12 col-xl-2 d-flex justify-content-center header__logo">
+        <div class="col-sm-6 col-md-12 col-xl-3 header__logo">
             <a href="#">Fibratela</a>            
         </div>
 
-        <div class="my-4 col-sm-12 col-md-3 col-xl-2 d-flex justify-content-center header__categorias">
+        <div class="col-sm-6 col-md-3 col-xl-2 header__categorias">
             <a href="#">Categorias
                 <i class="fa-solid fa-bars"></i>
             </a>            
         </div>
 
-        <div class="col-sm-12 col-md-4 col-xl-4 d-flex justify-content-center header_RedSocial">
+        <div class="col-sm-12 col-md-4 col-xl-3 header_RedSocial">
             <a href="#">
-                <i class="m-3 fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-facebook-f"></i>
             </a>
             <a href="#">
-                <i class="m-3 fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-instagram"></i>
             </a>
             <a href="#">
-                <i class="m-3 fa-brands fa-whatsapp"></i>
+                <i class="fa-brands fa-whatsapp"></i>
             </a>
         </div>
 
-        <div class="my-4 col-sm-12 col-md-2 col-xl-2 d-flex justify-content-center header_shopCar">
+        <div class="col-sm-12 col-md-2 col-xl-2 header_shopCar">
 
             <a href="#">
                 <i class="fa-solid fa-bag-shopping"></i>
-                $1000
+                $1
             </a>
             
         </div>
 
-        <div class="my-3 col-sm-12 col-md-2 col-xl-2 d-flex justify-content-center header_perfil">
+        <div class="col-sm-12 col-md-2 col-xl-2 header_perfil">
 
             <a href="#">    
                 <a href="#">
-                    <img src="" alt="Fotoperfil">
+                    <img src="views/assets/img/template/man-user-svgrepo-com.svg" alt="Fotoperfil">
                 </a>
             </a>
             
